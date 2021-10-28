b6972f3c8b24ae03c7ac2ce7b7f56c48e25bed2b
Joey Fang <josudoey@gmail.com>
2021-10-28 22:09:02 +0800

    refactor: update example

--- vue/pages/home/index.mjs
+++ vue/pages/home/index.mjs
@@ -1,39 +1,52 @@
 import { render, staticRenderFns } from './render.pug'
+import OpenImage from './open-image/index.mjs'
 export default {
   render,
   staticRenderFns,
+  components: {
+    OpenImage
+  },
   data: () => {
     return {
-      outputUrl: ''
-
+      outputUrl: '',
+      cropper: null
     }
   },
-  async mounted() {
+  async mounted () {
     import('cropperjs/dist/cropper.min.css')
-    const CropperModule = await import('cropperjs')
-    const Cropper = CropperModule.default
-    this.cropper = new Cropper(this.$refs.image, {
-      viewMode: 2,
-      dragMode: 'none',
-      minCropBoxWidth: 256,
-      minCropBoxHeight: 256,
-      aspectRatio: 1,
-      crop(event) {
-        console.log(event.detail.x);
-        console.log(event.detail.y);
-        console.log(event.detail.width);
-        console.log(event.detail.height);
-        console.log(event.detail.rotate);
-        console.log(event.detail.scaleX);
-        console.log(event.detail.scaleY);
-      },
-    })
+    this.initCropper()
   },
-  updated() {
+  updated () {
 
   },
   methods: {
-    crop() {
+    loadImg (img) {
+      this.cropper.destroy()
+      this.$refs.image.src = img.src
+      console.log(img.src)
+      this.initCropper()
+    },
+    async initCropper () {
+      const CropperModule = await import('cropperjs')
+      const Cropper = CropperModule.default
+      this.cropper = new Cropper(this.$refs.image, {
+        viewMode: 1,
+        dragMode: 'none',
+        minCropBoxWidth: 256,
+        minCropBoxHeight: 256,
+        aspectRatio: 1,
+        crop (event) {
+          console.log(event.detail.x)
+          console.log(event.detail.y)
+          console.log(event.detail.width)
+          console.log(event.detail.height)
+          console.log(event.detail.rotate)
+          console.log(event.detail.scaleX)
+          console.log(event.detail.scaleY)
+        }
+      })
+    },
+    crop () {
       this.outputUrl = this.cropper.getCroppedCanvas().toDataURL('image/png')
     }
   }
